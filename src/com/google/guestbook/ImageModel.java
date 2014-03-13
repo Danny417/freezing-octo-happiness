f438833df9d1643e96e6c417a359f5cbddcd463b
Randy Hsu <Randy@Randys-Mac-mini.local>
2014-03-13 00:24:34 -0700

    try again

--- src/com/google/guestbook/ImageModel.java
+++ src/com/google/guestbook/ImageModel.java
@@ -1,5 +1,65 @@
 package com.google.guestbook;
 
-public class ImageModel {
+import javax.jdo.annotations.Extension;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
 
+import com.google.appengine.api.datastore.Blob;
+import com.google.appengine.api.datastore.Key;
+
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
+public class ImageModel {
+	
+	
+	@PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Key key;
+	
+	@Persistent
+	private String ImageName;
+	
+	@Persistent
+    @Extension(vendorName="datanucleus", key="gae.unindexed", value="true")
+    private String imageType;
+
+	
+	@Persistent 
+	private Blob image;
+	
+	public Long getId(){
+		return key.getId();
+	}
+
+	public String getImageName(){
+		return ImageName;
+	}
+	
+	public String getImageType(){
+		return imageType;
+	}
+	
+	public byte[] getImage(){
+	      if (image == null) {
+	            return null;
+	        }
+
+	        return image.getBytes();
+	}
+	
+	public void setImageName(String Name){
+		this.ImageName = Name;
+	}
+	
+	public void setImageType(String imageType){
+		this.imageType = imageType;
+	}
+	
+	public void setImage(byte[] bytes){
+		this.image = new Blob(bytes);
+	}
+	
 }

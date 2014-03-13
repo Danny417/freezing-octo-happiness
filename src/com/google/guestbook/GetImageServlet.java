f5e3419168a786fc72651d5ea6e1cabebb925d0d
Randy Hsu <Randy@Randys-Mac-mini.local>
2014-03-13 01:00:02 -0700

    Servlets

--- src/com/google/guestbook/GetImageServlet.java
+++ src/com/google/guestbook/GetImageServlet.java
@@ -1,5 +1,66 @@
 package com.google.guestbook;
 
-public class GetImageServlet {
+import java.io.IOException;
+import java.util.List;
 
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Query;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class GetImageServlet extends HttpServlet {
+	 @Override
+	    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	            throws IOException {
+		 
+		    //MAYBE NOT TITLE HERE
+	        String title = req.getParameter("title");
+	        ImageModel image = getImage(title);
+
+	        if (image != null && image.getImageType() != null &&
+	                image.getImage() != null) {
+	            // Set the appropriate Content-Type header and write the raw bytes
+	            // to the response's output stream
+	            resp.setContentType(image.getImageType());
+	            resp.getOutputStream().write(image.getImage());
+	        } else {
+	            // If no image is found with the given title, redirect the user to
+	            // a static image
+	            resp.sendRedirect("/static/noimage.jpg");
+	        }
+	    }
+
+	    //...
+	 private ImageModel getImage(String title) {
+		    PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory();
+            PersistenceManager pm = pmf.getPersistenceManager();
+
+
+		    // Search for any ImageModel object with the passed-in title; limit the number
+		    // of results returned to 1 since there should be at most one Image with
+		    // a given title
+		    //NEED CHANGES!!!!!!!!!!!
+		    Query query = pm.newQuery(ImageModel.class, "title == titleParam");
+		    query.declareParameters("String titleParam");
+		    query.setRange(0, 1);
+
+		    try {
+		        List<ImageModel> results = (List<ImageModel>) query.execute(title);
+		        if (results.iterator().hasNext()) {
+		            // If the results list is non-empty, return the first (and only)
+		            // result
+		            return results.get(0);
+		        }
+		    } finally {
+		        query.closeAll();
+		        pm.close();
+		    }
+
+		    return null;
+		}
 }
--- src/com/google/guestbook/StoreImageServlet.java
+++ src/com/google/guestbook/StoreImageServlet.java
@@ -1,5 +1,65 @@
 package com.google.guestbook;
 
-public class StoreImageServlet {
+import java.io.IOException;
+import java.net.URL;
 
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.urlfetch.HTTPHeader;
+import com.google.appengine.api.urlfetch.HTTPResponse;
+import com.google.appengine.api.urlfetch.URLFetchService;
+import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
+
+public class StoreImageServlet extends HttpServlet {
+	
+	@Override 
+	  public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	            throws IOException {
+	        URLFetchService fetchService =
+	            URLFetchServiceFactory.getURLFetchService();
+	     // Fetch the image at the location given by the url query string parameter
+	        HTTPResponse fetchResponse = fetchService.fetch(new URL(
+	                req.getParameter("url")));
+	        
+	        String fetchResponseContentType = null;
+	        for (HTTPHeader header : fetchResponse.getHeaders()) {
+	            // For each request header, check whether the name equals
+	            // "Content-Type"; if so, store the value of this header
+	            // in a member variable
+	            if (header.getName().equalsIgnoreCase("content-type")) {
+	                fetchResponseContentType = header.getValue();
+	                break;
+	            }
+	        }
+	        
+	        if (fetchResponseContentType != null) {
+	            // Create a new image instance
+	            ImageModel image = new ImageModel();
+	            
+	            //MAYBE NOT TITLE HERE!
+	            image.setImageName(req.getParameter("title"));
+	            image.setImageType(fetchResponseContentType);
+
+	            // Set the image by passing in the bytes pulled
+	            // from the image fetched via the URL Fetch service
+	            image.setImage(fetchResponse.getContent());
+
+	            //...
+
+	            	///MAYBE WORKING!!!!!!!!!!!!!!!!!     
+	            PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory();
+	            PersistenceManager pm = pmf.getPersistenceManager();
+	            try {
+	                // Store the image in App Engine's datastore
+	                pm.makePersistent(image);
+	            } finally {
+	                pm.close();
+	            }
+	        }
+	}
 }

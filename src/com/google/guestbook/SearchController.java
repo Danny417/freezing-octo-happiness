28eefc99164510936d3e5d296f9c55f11e4321c6
Randy Hsu <Randy@Randys-Mac-mini.local>
2014-04-20 21:07:27 -0700

    Add saving image to parkingSpot

--- src/com/google/guestbook/SearchController.java
+++ src/com/google/guestbook/SearchController.java
@@ -1,6 +1,7 @@
 package com.google.guestbook;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 
 import javax.jdo.PersistenceManager;
@@ -11,18 +12,51 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.appengine.api.datastore.Email;
-import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.urlfetch.HTTPHeader;
+import com.google.appengine.api.urlfetch.HTTPResponse;
+import com.google.appengine.api.urlfetch.URLFetchService;
+import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
-import com.google.gson.Gson;
 
 public class SearchController extends HttpServlet{
 	
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
     	//temp data to have some parking spots
+		
+	//Use URLFetchService to get image
+	URLFetchService fetchService = URLFetchServiceFactory.getURLFetchService();
+	HTTPResponse fetchResponse = fetchService.fetch(new URL(
+            req.getParameter("url")));
+	String 	fetchResponseContentType = null;
+	for (HTTPHeader header : fetchResponse.getHeaders()) {
+        // For each request header, check whether the name equals
+        // "Content-Type"; if so, store the value of this header
+        // in a member variable
+        if (header.getName().equalsIgnoreCase("content-type")) {
+            fetchResponseContentType = header.getValue();
+            break;
+        }
+    }
+	
     	PersistenceManager pm = PMF.get().getPersistenceManager();
     	ParkingSpotModel ps = new ParkingSpotModel(115.0, "2 address st.", 49.279387, -122.958822);
+    
+    	//Get the image with attribute "title"
+    	if (fetchResponseContentType != null){
+    		ImageModel image = new ImageModel();
+    		image.setImageName(req.getParameter("title"));
+            image.setImageType(fetchResponseContentType);
+
+            // Set the image by passing in the bytes pulled
+            // from the image fetched via the URL Fetch service
+            image.setImage(fetchResponse.getContent());
+            
+            //Add to the ParkSpotModel for saving
+            ps.setParkingImage(image);
+    	}
+    	
     	UserModel host = new UserModel("test user1", new Email("2testUser1@test.com"));
     	host.addParkingSpot(ps);
     	try {

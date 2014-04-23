146729f9f13a3368e5ffc9a2b0e6159ab6d6ded0
Diana Sutandie <dianasutandie@gmail.com>
2014-04-22 19:01:42 -0700

    small changes to userprof

--- war/js/UserProfile.js
+++ war/js/UserProfile.js
@@ -4,7 +4,7 @@ window.onload = function() {
 	
 	
 	// initialize the owned table
-	var ownedContent = "<thead><tr><th>Parking Spot ID</th>"+
+	var ownedContent = "<thead><tr><th>#</th>"+
 					"<th>Location</th>"+
 					"<th>Price</th>"+
 					"<th>Status</th></tr></thead>";
@@ -13,7 +13,7 @@ window.onload = function() {
 		
 		for (var i=0; i < ownedSpots.length; i++){
 			ownedContent = ownedContent + "<tr>" +
-							"<th>" + ownedSpots[i].parkingSpotID + "</th>" +
+							"<th>" + i + "</th>" +
 							"<th>" + ownedSpots[i].address + "</th>" + 
 							"<th>" + ownedSpots[i].price + "</th>" +
 							"<th></th></tr>";
